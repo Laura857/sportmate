710ea0fb81c5541acfaa389702680357c45c20c9
Laura <laura@MacBook-Pro-de-Laura-3.home>
2021-11-29 20:51:18 +0100

    add spaces

--- src/main/java/com/example/sportmate/entity/Activity.java
+++ src/main/java/com/example/sportmate/entity/Activity.java
@@ -4,7 +4,17 @@ import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 
-public record Activity(@Id Integer id, boolean isEvent, String activityName, LocalDate activityDate, Integer creator,
-                       String address, String longitude, String latitude, Integer participant, Integer sport,
-                       Integer activityLevel, LocalDate created, LocalDate updated) {
+public record Activity(@Id Integer id,
+                       boolean isEvent,
+                       String activityName,
+                       LocalDate activityDate,
+                       Integer creator,
+                       String address,
+                       String longitude,
+                       String latitude,
+                       Integer participant,
+                       Integer sport,
+                       Integer activityLevel,
+                       LocalDate created,
+                       LocalDate updated) {
 }
