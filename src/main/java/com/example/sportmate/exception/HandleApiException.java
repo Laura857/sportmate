5d4943dc8db477bc9a2660ed64c2663f568bfff7
laura <laura.apremont@hotmail.fr>
2022-02-18 12:34:22 +0100

    add flyway script

--- src/main/java/com/example/sportmate/exception/HandleApiException.java
+++ src/main/java/com/example/sportmate/exception/HandleApiException.java
@@ -1,8 +1,10 @@
 package com.example.sportmate.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -18,6 +20,16 @@ public class HandleApiException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {AuthenticationException.class, AuthenticationException.class})
     protected ResponseEntity<Object> handleAuthenticationException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {InvalidFormatException.class, InvalidFormatException.class})
+    protected ResponseEntity<Object> handleInvalidFormatException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
